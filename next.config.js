5074f453592fb9b2ef0d12145bfe7744c6c5b581
Kertf22 <paulo.hercilio2015@gmail.com>
2021-10-31 16:00:53 -0300

    Now it will work

--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
 module.exports = {
-    webpack5: false,
-    webpack: (config) => {
-      config.resolve.fallback = { fs: false };
-  
-      return config;
-    },
+
     images: {
       domains: ['ufs-project.s3.amazonaws.com'],
     },
